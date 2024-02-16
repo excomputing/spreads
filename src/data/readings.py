e4dd537b05841dcef84b9274445dc51a2ea2dec8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-16 19:43:29 +0000

    Update src/data/readings.py

--- src/data/readings.py
+++ src/data/readings.py
@@ -3,10 +3,10 @@ import os
 
 import dask.dataframe as ddf
 import numpy as np
-
-import src.elements.s3_parameters as s3p
+import pandas as pd
 
 import src.algorithms.distributions
+import src.elements.s3_parameters as s3p
 
 
 class Readings:
@@ -25,6 +25,22 @@ class Readings:
         self.__rename = {0.1: 'lower_decile', 0.25: 'lower_quartile', 0.5: 'median',
                          0.75: 'upper_quartile', 0.9: 'upper_decile'}
 
+    def __calculations(self, frame: ddf.DataFrame) -> pd.DataFrame:
+
+        computations = frame[['sequence_id', 'date', 'measure']].groupby(
+            by=['sequence_id', 'date']).apply(self.__distributions.quantiles, meta=self.__meta)
+        calculations = computations.compute(scheduler='processes')
+        calculations.reset_index(drop=False, inplace=True)
+
+        return calculations
+
+    def __persist(self, blob: pd.DataFrame) -> bool:
+
+        logging.log(level=logging.INFO,
+                    msg=blob.rename(columns=self.__rename).head())
+
+        return True
+
     def exc(self, s3_keys: list):
         """
 
@@ -38,11 +54,7 @@ class Readings:
 
         for node in nodes:
 
+            logging.log(level=logging.INFO, msg=f'\n\n{node}')
             frame: ddf.DataFrame = ddf.read_csv(node)
-
-            computations = frame[['sequence_id', 'date', 'measure']].groupby(
-                by=['sequence_id', 'date']).apply(self.__distributions, meta=self.__meta)
-            calculations = computations.compute(scheduler='processes')
-            calculations.reset_index(drop=False, inplace=True)
-
-            logging.log(level=logging.INFO, msg=calculations)
+            calculations = self.__calculations(frame=frame)
+            self.__persist(blob=calculations)
