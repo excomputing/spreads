73c29c6ab1baf953619a88b1bed8018641366710
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-17 20:59:39 +0000

    Update src/s3/unload.py

--- src/s3/unload.py
+++ src/s3/unload.py
@@ -3,32 +3,28 @@ Module unload.py
 """
 import io
 
-import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 
 
 class Unload:
 
-    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+    def __init__(self, service: sr.Service):
         """
 
         :param service: A suite of services for interacting with Amazon Web Services.
-        :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
-                              name, bucket name, etc.
         """
 
-        self.__s3_parameters: s3p.S3Parameters = s3_parameters
-        self.__s3_resource = service.s3_resource
         self.__s3_client = service.s3_client
 
-    def exc(self, key_name: str):
+    def exc(self, bucket_name: str, key_name: str):
         """
 
-        :param key_name: The S3 path of the data file, excluding the bucket name, including the file name..
+        :param bucket_name:
+        :param key_name: The S3 path of the data file, excluding the bucket name, including the file name.
         :return:
         """
 
-        blob = self.__s3_client.get_object(Bucket=self.__s3_parameters.bucket_name, Key=key_name)
+        blob = self.__s3_client.get_object(Bucket=bucket_name, Key=key_name)
         buffer = io.StringIO(blob['Body'].read().decode('utf-8'))
 
         return buffer
