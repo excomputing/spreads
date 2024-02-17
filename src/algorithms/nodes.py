e425528415af0d9ab7b68512e649966450d01a64
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-17 20:57:18 +0000

    Update src/algorithms/nodes.py

--- src/algorithms/nodes.py
+++ src/algorithms/nodes.py
@@ -2,28 +2,25 @@ import os
 
 import numpy as np
 
-import src.elements.s3_parameters as s3p
-
 
 class Nodes:
 
-    def __init__(self, s3_parameters: s3p.S3Parameters):
+    def __init__(self):
+        """
+        Constructor
         """
 
-        :param s3_parameters:
-        """
-
-        self.__s3_parameters = s3_parameters
-
-    def exc(self, s3_keys: list):
+    @staticmethod
+    def exc(s3_keys: list, bucket_name: str):
         """
 
         :param s3_keys:
+        :param bucket_name:
         :return:
         """
 
         strings = [os.path.dirname(s3_key) for s3_key in s3_keys]
         paths = np.unique(np.array(strings))
-        nodes = [f's3://{self.__s3_parameters.bucket_name}/{path}/*.csv' for path in paths]
+        nodes = [f's3://{bucket_name}/{path}/*.csv' for path in paths]
 
         return nodes
