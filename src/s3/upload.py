75359c1f8e2269a3f51b087f2d316d7e9f6c2c2f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-18 02:28:45 +0000

    Update src/s3/upload.py

--- src/s3/upload.py
+++ src/s3/upload.py
@@ -1,13 +1,10 @@
 """
 Module upload.py
 """
-import io
 
 import boto3
 import botocore.exceptions
-import pandas as pd
 
-import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 
 
@@ -21,20 +18,22 @@ class Upload:
           * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/Object.html
     """
 
-    def __init__(self, service: sr.Service, bucket_name: str):
+    def __init__(self, service: sr.Service, bucket_name: str, metadata: dict):
         """
 
-        :param service:
+        :param service: A suite of services for interacting with Amazon Web Services.
+        :param bucket_name: The name of the Amazon S3 bucket that a data set is being delivered to.
+        :param metadata: The metadata of the data
         """
 
-        self.__bucket_name = bucket_name
         self.__s3_resource: boto3.session.Session.resource = service.s3_resource
+        self.__bucket_name = bucket_name
+        self.__metadata = metadata
 
-    def bytes(self, buffer: bytes, metadata: dict, key_name: str) -> bool:
+    def bytes(self, buffer: bytes, key_name: str) -> bool:
         """
 
         :param buffer: The data that will be delivered to Amazon S3
-        :param metadata: The metadata of the data
         :param key_name: The key name of the data -> {}/{}/{}.extension
         :return:
         """
@@ -45,7 +44,8 @@ class Upload:
         try:
             bucket.put_object(
                 Body=buffer,
-                Key=key_name, Metadata=metadata)
+                Key=key_name,
+                Metadata=self.__metadata)
             return True or False
         except botocore.exceptions.ClientError as err:
             raise Exception(err) from err
