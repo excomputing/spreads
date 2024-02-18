beaf86fa6c8f111f5759306355546e557d87dec3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-18 02:28:00 +0000

    Update src/algorithms/persist.py: In Progress

--- src/algorithms/persist.py
+++ src/algorithms/persist.py
@@ -4,23 +4,25 @@ Module persist.py
 import logging
 import os.path
 
-import numpy as np
-import pandas as pd
-
-import src.functions.objects
 import config
+import src.functions.objects
 
 
 class Persist:
     """
-
     This class exports the daily quantiles calculations of a telemetric device, i.e., of a pollutant, to a JSON file.  The
     JSON file includes a summary of the underlying raw data's characteristics.
     """
 
-    def __init__(self, ):
+    def __init__(self):
+        """
+        Constructor
+        """
 
+        # The local storage parent directory
         self.__storage = config.Config().storage
+
+        # An instance for writing JSON files
         self.__objects = src.functions.objects.Objects()
 
         # Logging: If necessary, set force = True
@@ -28,18 +30,26 @@ class Persist:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def __write(self, nodes: dict) -> str:
+    def __local(self, nodes: dict) -> str:
+        """
+
+        :param nodes:
+        :return:
+        """
 
         dictionary = nodes['attributes']
+        name = f"pollutant_{dictionary['pollutant_id']}_station_{dictionary['station_id']}.json"
 
-        message = self.__objects.write(
-            nodes=nodes,
-            path=os.path.join(self.__storage, f"pollutant_{dictionary['pollutant_id']}_station_"
-                                              f"{dictionary['station_id']}.json"))
+        message = self.__objects.write(nodes=nodes, path=os.path.join(self.__storage, name))
 
         return message
 
     def exc(self, nodes: dict):
+        """
 
-        message = self.__write(nodes=nodes)
+        :param nodes:
+        :return:
+        """
+
+        message = self.__local(nodes=nodes)
         self.__logger.info(message)
