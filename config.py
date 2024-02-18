074e5341910ce23d782a35c72c237c014bf3bacd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-18 02:26:52 +0000

    Update config.py: In Progress

--- config.py
+++ config.py
@@ -11,3 +11,8 @@ class Config:
         """
 
         self.storage = os.path.join(os.getcwd(), 'warehouse')
+
+        self.metadata = {
+            'attributes': 'Brief [telemetric device] raw data source details.',
+            'columns': 'The columns of each data instance.',
+            'data': 'The data instances.  Each instance records the quantiles and extrema calculations of a day.'}
