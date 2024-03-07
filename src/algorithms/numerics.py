88e908880994e6d282eb57f34f41077b10f7774b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-07 21:47:00 +0000

    Update src/algorithms/numerics.py: In Progress

--- src/algorithms/numerics.py
+++ src/algorithms/numerics.py
@@ -23,16 +23,31 @@ class Numerics:
         self.__data = cudf.from_pandas(frame)
 
         # The quantile points
-        self.__decimals = np.array([0.1, 0.25, 0.5, 0.75,0.9])
+        self.__decimals: np.ndarray[float] = np.array([0.1, 0.25, 0.5, 0.75, 0.9])
+
+        # Dictionary
+        self.__dictionary = {0.1: 'lower_decile', 0.25: 'lower_quartile', 0.5: 'median',
+                             0.75: 'upper_quartile', 0.9: 'upper_decile'}
 
     def __quantiles(self):
         """
+        Unfortunately groupby.quantile(q=np.array(...)) is not yet functioning
         
         :return
         """
 
-        calc: cudf.DataFrame = self.__data[['sequence_id', 'date', 'measure']].groupby(
-            by=['sequence_id', 'date']).quantile(q=self.__decimals)
+        lower_decile = lambda x: x.quantile(0.1); lower_decile.__name__ = 'lower_decile'
+        lower_quartile = lambda x: x.quantile(0.25); lower_quartile.__name__ = 'lower_quartile'
+        median = lambda x: x.quantile(0.5); median.__name__ = 'median'
+        upper_quartile = lambda x: x.quantile(0.75); upper_quartile.__name__ = 'upper_quartile'
+        upper_decile = lambda x: x.quantile(0.9); upper_decile.__name__ = 'upper_decile'
+
+        blob = self.__data.copy()[['sequence_id', 'date', 'measure']]
+        
+        calc = blob.groupby(by=['sequence_id', 'date']).agg(
+            [lower_decile, lower_quartile, median, upper_quartile, upper_decile])
+        
+        calc.reset_index(drop=False, inplace=True)
 
         return calc
 
@@ -43,10 +58,9 @@ class Numerics:
         """
 
         calc: cudf.DataFrame = self.__data[['sequence_id', 'date', 'measure']].groupby(
-            by=['sequence_id', 'date']).agg(minimum=('measure', min), maximum=('measure', max))
+            by=['sequence_id', 'date']).agg(['min', 'max'])
 
         return calc
-
 
     def exc(self):
         """
@@ -54,7 +68,12 @@ class Numerics:
         :return
         """
 
-        left: cudf.DataFrame = self.__quantiles()
+        left = self.__quantiles()
+        print(type(left))
+        print(left)
+
         right:cudf.DataFrame = self.__extrema()
-        calculations = left.copy().merge(right.copy(), on=['sequence_id', 'date'], how='inner')
-        logging.log(level=logging.INFO, msg=calculations)
+        logging.log(level=logging.INFO, msg=right)
+
+        # calculations = left.copy().merge(right.copy(), on=['sequence_id', 'date'], how='inner')
+        # logging.log(level=logging.INFO, msg=calculations)
