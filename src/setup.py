5a0cce17aa2792be834069719de5b3ce8d6caf3e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-08 22:43:23 +0000

    Update src/setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,6 @@
+"""
+Module setup.py
+"""
 
 import config
 
@@ -7,7 +10,14 @@ import src.s3.bucket
 import src.s3.objects
 import src.functions.directories
 
+
 class Setup:
+    """
+    Notes
+    -----
+
+    Prepares local and cloud depositories.    
+    """
 
     def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters) -> None:
         """
@@ -20,29 +30,48 @@ class Setup:
         self.__service: sr.Service = service
         self.__s3_parameters: s3p.S3Parameters = s3_parameters
 
+        # Configurations
         self.__configurations = config.Config()
 
-    def __local(self):
+    def __local(self) -> None:
+        """
+        
+        :return:
+            None
+        """
         
         directories = src.functions.directories.Directories()
         directories.cleanup(path=self.__configurations.storage)
         directories.create(path=self.__configurations.storage)
 
-    def __cloud(self):
+    def __cloud(self) -> None:
+        """
+        Preparing the externally facing Amazon S3 (Simple Storage Service) bucket
+        
+        :return:
+            None
+        """
 
-        # Preparing the externally facing Amazon S3 bucket & path
-        bucket = src.s3.bucket.Bucket(service=self.__service, location_constraint=self.__s3_parameters.location_constraint, 
-                                      bucket_name=self.__s3_parameters.external)
+        # If the target bucket exists, create an object of its content
         objects = src.s3.objects.Objects(service=self.__service, bucket_name=self.__s3_parameters.external)
-
         iterable = objects.filter(prefix=self.__s3_parameters.path_external_quantiles)
 
+        # Create a bucket object
+        bucket = src.s3.bucket.Bucket(service=self.__service, location_constraint=self.__s3_parameters.location_constraint, 
+                                      bucket_name=self.__s3_parameters.external)
+        
+        # If the bucket exists, empty it, otherwise create it
         if bucket.exists():
             iterable.delete()
         else:
             bucket.create()
 
-    def exc(self):
+    def exc(self) -> None:
+        """
+        
+        :return:
+            None
+        """
 
         self.__local()
         self.__cloud()    
