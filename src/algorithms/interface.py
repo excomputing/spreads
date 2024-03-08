6fb6a9f748385fd694485a0d342aa4a2f474be76
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-08 20:47:12 +0000

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -87,15 +87,15 @@ class Interface:
 
             # A collection of a device's timeseries data; retrieved in parallel
             frame: ddf.DataFrame = ddf.read_csv(branch)
-            src.algorithms.numerics.Numerics(frame=frame.compute(scheduler="processes")).exc()
+            data: pd.DataFrame = src.algorithms.numerics.Numerics(frame=frame.compute(scheduler="processes")).exc()
 
             # Calculations
-            quantiles: pd.DataFrame = self.__quantiles(frame=frame)
-            extrema: pd.DataFrame = self.__extrema(frame=frame)
+            # quantiles: pd.DataFrame = self.__quantiles(frame=frame)
+            # extrema: pd.DataFrame = self.__extrema(frame=frame)
 
             # Merge
-            data: pd.DataFrame = quantiles.copy().merge(extrema.copy(), on=['sequence_id', 'date'], how='inner')
-            data.rename(columns=self.__rename, inplace=True)
+            # data: pd.DataFrame = quantiles.copy().merge(extrema.copy(), on=['sequence_id', 'date'], how='inner')
+            # data.rename(columns=self.__rename, inplace=True)
 
             # Structure
             nodes: dict = structure.exc(data=data)
