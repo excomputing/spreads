5c1488adcbb74c3c0ee7b4ceef95aeb609c4a5a2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-19 23:41:26 +0000

    Update src/algorithms/points.py

--- src/algorithms/points.py
+++ src/algorithms/points.py
@@ -12,21 +12,42 @@ class Points:
         self.__logger = logging.getLogger(__name__)
 
     def lower_decile(self, x):
+        """
+        
+        :param x: A time series.
+        """
 
         return x.quantile(0.10)
     
     def lower_quartile(self, x):
+        """
+        
+        :param x: A time series.
+        """
 
         return x.quantile(0.25)
     
     def median(self, x):
+        """
+        
+        :param x: A time series.
+        """
+        
 
         return x.quantile(0.50)
     
     def upper_quartile(self, x):
+        """
+        
+        :param x: A time series.
+        """
 
         return x.quantile(0.75)
     
     def upper_decile(self, x):
+        """
+        
+        :param x: A time series.
+        """
 
         return x.quantile(0.90)
