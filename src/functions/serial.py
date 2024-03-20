93078b15e94fe50211509ea01ff38acb14b83c6b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-20 20:42:01 +0000

    Update src/functions/serial.py

--- src/functions/serial.py
+++ src/functions/serial.py
@@ -17,7 +17,6 @@ class Serial:
         """
         Constructor
         """
-        pass
 
     @staticmethod
     def get_dictionary(uri: str) -> dict:
@@ -27,8 +26,8 @@ class Serial:
         :return:
         """
 
-        with open(file=uri, mode='r') as stream:
+        with open(file=uri, mode='r', encoding='utf-8') as stream:
             try:
                 return yaml.load(stream=stream, Loader=yaml.CLoader)
             except yaml.YAMLError as err:
-                raise Exception(err) from err
+                raise err from err
