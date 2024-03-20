347a67bbf23e13b9f3475a178996fadf0cb479ed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-20 20:40:43 +0000

    Update src/functions/objects.py

--- src/functions/objects.py
+++ src/functions/objects.py
@@ -54,17 +54,13 @@ class Objects:
             response = requests.get(url=url, timeout=600)
             response.raise_for_status()
         except requests.exceptions.Timeout as err:
-            logging.log(level=logging.INFO, msg=f"TIME OUT: {url.split('timeseries')[1]}")
-            raise err from err
-        except requests.exceptions.HTTPError as err:
-            raise f'HTTP Error: {err}' from err
+            raise f"TIME OUT: {url.split('timeseries')[1]}\n{err}" from err
         except Exception as err:
             raise err from err
 
         if response.status_code == 200:
             return response.json()
-        else:
-            raise Exception(f'Failure code: {response.status_code}')
+        raise f'Failure code: {response.status_code}'
 
     @staticmethod
     def read(uri: str) -> dict:
